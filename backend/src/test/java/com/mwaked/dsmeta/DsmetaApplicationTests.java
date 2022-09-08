819fb1281e78a6196ec4ef4331255a9cc69d9d5c
Matheus Waked Franca <matheus363@hotmail.com>
2022-09-08 15:05:53 -0300

    Project created

---
+++ backend/src/test/java/com/mwaked/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwaked.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
