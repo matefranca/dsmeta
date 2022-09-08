d3999feb5046a0015b346f9b1cb32899c6b0292d
Matheus Waked Franca <matheus363@hotmail.com>
2022-09-08 15:40:45 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import NotificationButton from './components/NotificationButton'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <NotificationButton />
+    </>
   )
 }
 
